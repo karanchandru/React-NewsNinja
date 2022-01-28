832d03116ee21a523a98070d4c571ff3f3b7b183
krupa02 <33410064+krupa02@users.noreply.github.com>
2022-01-29 01:33:10 +0530

    29/1/2022

--- src/components/News.js
+++ src/components/News.js
@@ -8,16 +8,45 @@ export class News extends Component {
     // console.log("hello I'm here in constructor");
     this.state={
       articles:[],
-      loading:false
+      loading:false,
+      page:1
     }
   }
 
  //lifecycle method
  async componentDidMount(){
-     let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249";
+     let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=1&pageSize=20";
      let data=await fetch(url);
      let parsedData=await data.json();
-     this.setState({articles:parsedData.articles})
+     this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
+ }
+ 
+ handlePrevClick = async ()=>{
+     console.log("prev");
+     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=${this.state.page -1}&pageSize=20`;
+     let data=await fetch(url);
+     let parsedData=await data.json();     
+     this.setState({
+         page:this.state.page -1,
+         articles:parsedData.articles
+     })
+
+ }
+ handleNextClick= async ()=>{
+    //console.log("next");
+     if(Math.ceil(this.state.page +1 > this.state.totalResults/20)){
+        console.log(" No next");
+     }
+     else{
+     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=${this.state.page +1}&pageSize=20`;
+     let data=await fetch(url);
+     let parsedData=await data.json();     
+     this.setState({
+         page:this.state.page +1,
+         articles:parsedData.articles
+     })
+    }
+
  }
 
   render() {
@@ -37,6 +66,10 @@ export class News extends Component {
           }
         )}
 
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page <=1} type="button" onClick={this.handlePrevClick} className="btn btn-dark">&laquo; Previous</button>
+        <button type="button" onClick={this.handleNextClick} className="btn btn-dark">Next &raquo;</button>
         </div>
     </div>
     )
