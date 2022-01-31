8c4eedf152fbae464239bfaee7b09807a818f51e
Karan C <33410064+krupa02@users.noreply.github.com>
2022-02-01 00:54:57 +0530

    loading logo

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -15,35 +16,41 @@ export class News extends Component {
 
  //lifecycle method
  async componentDidMount(){
-     let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=1&pageSize=20";
+     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=1&pageSize=${this.props.pageSize}`;
+     this.setState({loading:true});
      let data=await fetch(url);
      let parsedData=await data.json();
-     this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
+     this.setState({
+       articles:parsedData.articles,
+       totalResults:parsedData.totalResults,
+       loading: false
+      })
  }
  
  handlePrevClick = async ()=>{
    
-     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=${this.state.page -1}&pageSize=20`;
+     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=${this.state.page -1}&pageSize=${this.props.pageSize}`;
+     this.setState({loading:true});
      let data=await fetch(url);
      let parsedData=await data.json();     
      this.setState({
          page:this.state.page -1,
-         articles:parsedData.articles
+         articles:parsedData.articles,
+         loading: false
      })
 
  }
  handleNextClick= async ()=>{
     console.log("next");
-     if(Math.ceil(this.state.page +1 > this.state.totalResults/20)){
-        console.log(" No next");
-     }
-     else{
-     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=${this.state.page +1}&pageSize=20`;
+     if(!(Math.ceil(this.state.page +1 > this.state.totalResults/this.props.pageSize))){
+     let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=82375203c639443fb887e14666d06249&page=${this.state.page +1}&pageSize=${this.props.pageSize}`;
+     this.setState({loading:true});
      let data=await fetch(url);
      let parsedData=await data.json();     
      this.setState({
          page:this.state.page +1,
-         articles:parsedData.articles
+         articles:parsedData.articles,
+         loading: false
      })
     }
 
@@ -52,10 +59,10 @@ export class News extends Component {
   render() {
     return (
     <div className="container my-3">
-        <h2>NewsNenja  -Top Headline</h2>
-        
+        <h1 className="text-center">NewsNenja  -Top Headline</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row my-4">
-        {this.state.articles.map(
+        {!this.state.loading && this.state.articles.map(
           (element)=>{
             return <div className="col-md-4" key={element.url}>
             <NewsItem  title={element.title ? element.title.slice(0,45) : ""} 
@@ -69,7 +76,8 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
         <button disabled={this.state.page <=1} type="button" onClick={this.handlePrevClick} className="btn btn-dark">&laquo; Previous</button>
-        <button type="button" onClick={this.handleNextClick} className="btn btn-dark">Next &raquo;</button>
+        <button disabled={this.state.page +1 > this.state.totalResults/this.props.pageSize} 
+        type="button" onClick={this.handleNextClick} className="btn btn-dark">Next &raquo;</button>
         </div>
     </div>
     )
